221ee4c3f5054a4c55349dfa6f9bc0a4277e2155
smhulme <smhulme101@gmail.com>
2025-10-09 09:07:50 -0600

    added in the template for the class for the user registration

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+
+import javax.naming.Context;
+import java.util.Map;
 
 public class Server {
 
@@ -11,8 +15,14 @@ public class Server {
 
         server.delete("db", ctx -> ctx.result("{}"));
         server.post("user", ctx -> ctx.result("{ \"username\":\"\", \"authToken\":\"\" }"));
-        
+    }
 
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        // call service to register
+        var res = Map.of("username", req.get("username"), "authToken", "xyz");
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
