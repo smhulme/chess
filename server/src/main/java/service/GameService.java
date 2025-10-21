119eea204547bef069c971ae01df86de8010506c
smhulme <smhulme101@gmail.com>
2025-10-21 11:54:50 -0600

    implemented JoinGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -4,6 +4,7 @@ import dataaccess.*;
 import datamodel.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Random;
 
 public class GameService {
@@ -43,10 +44,43 @@ public class GameService {
     }
 
     public boolean joinGame(String authToken, int gameID, String color) throws UnauthorizedException, BadRequestException {
-        
 
+        RegisterResponse registerResponse;
+        GameData gameData;
 
+        try {
+            registerResponse = authAccess.getAuth(authToken);
+        } catch (DataAccessException e) {
+            throw new UnauthorizedException();
+        }
 
+        try {
+            gameData = gameAccess.getGame(gameID);
+        } catch (DataAccessException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+
+        String whiteUser = gameData.whiteUsername();
+        String blackUser = gameData.blackUsername();
+
+        if (Objects.equals(color, "WHITE")) {
+            if (whiteUser != null) return false; // Spot taken
+            else whiteUser = registerResponse.username();
+        } else if (Objects.equals(color, "BLACK")) {
+            if (blackUser != null) return false; // Spot taken
+            else blackUser = registerResponse.username();
+        } else if (color == null || color.isEmpty()) { // Check for null or empty string explicitly
+            throw new BadRequestException("Error: Player color cannot be null or empty");
+        } else if (!color.equals("WHITE") && !color.equals("BLACK")) { // Check for other invalid colors like "GREEN"
+            throw new BadRequestException("%s is not a valid team color".formatted(color));
+        }
+        // If we reach here, color must have been valid (WHITE/BLACK) and the spot free.
+        gameAccess.updateGame(new GameData(gameID, whiteUser, blackUser, gameData.gameName(), gameData.game()));
         return true;
+        
+    }
+
+    public void clear() {
+        gameAccess.clear();
     }
 }
