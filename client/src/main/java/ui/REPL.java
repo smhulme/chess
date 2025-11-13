dc7a809d07d63c1802846e3ef27762676a7c648e
smhulme <smhulme101@gmail.com>
2025-11-12 23:39:38 -0700

    added REPL class and attatched it to main

--- client/src/main/java/ui/REPL.java
+++ client/src/main/java/ui/REPL.java
@@ -1,4 +1,35 @@
 package ui;
 
+import client.ResponseException;
+import java.util.Scanner;
+
 public class REPL {
+    private final ChessClient client;
+
+    public REPL(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to the 240 Chess Client. Type Help to get started.");
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + EscapeSequences.SET_TEXT_COLOR_WHITE + " >>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
+    }
 }
