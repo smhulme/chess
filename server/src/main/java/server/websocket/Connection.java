52d71d7d70db8e103503373b10b8891573cb3a1d
smhulme <smhulme101@gmail.com>
2025-11-30 18:44:18 -0700

    implement Connection class to hold session info

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,5 +1,15 @@
 package server.websocket;
 
+import io.javalin.websocket.WsContext;
+
 public class Connection {
-    
+    public String authToken;
+    public Integer gameID;
+    public WsContext session;
+
+    public Connection(String authToken, Integer gameID, WsContext session) {
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.session = session;
+    }
 }
